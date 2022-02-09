888bd64470c3a6ec5aa9e07b9a85f1fc8fdb8234
dev-jamesmin <dev.jamesmin@gmail.com>
2022-02-09 17:49:38 +0900

    [kmm] 클론 프로젝트 메인 빌드 파일 설정 적용.

--- Kmm/Sample/build.gradle.kts
+++ Kmm/Sample/build.gradle.kts
@@ -1,22 +1,47 @@
 buildscript {
+    val kotlinVersion: String by project
+//    println(kotlinVersion)
+
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven(uri("https://plugins.gradle.org/m2/")) // For kotlinter-gradle
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.android.tools.build:gradle:7.1.1")
+//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.10")
+
+        with(Deps.Gradle) {
+            classpath(sqlDelight)
+            classpath(shadow)
+            classpath(kotlinter)
+            classpath(gradleVersionsPlugin)
+            classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin:${Versions.kmpNativeCoroutinesVersion}")
+        }
     }
 }
 
 allprojects {
+    apply(plugin = "org.jmailen.kotlinter")
+
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
+        maven(url = "https://jitpack.io")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven")
     }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+//tasks.register("clean", Delete::class) {
+//    delete(rootProject.buildDir)
+//}
+
+// On Apple Silicon we need Node.js 16.0.0
+// https://youtrack.jetbrains.com/issue/KT-49109
+rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class) {
+    rootProject.the(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension::class).nodeVersion = "16.0.0"
 }
--- Kmm/Sample/settings.gradle.kts
+++ Kmm/Sample/settings.gradle.kts
@@ -9,3 +9,4 @@ pluginManagement {
 rootProject.name = "kmmSample"
 include(":androidApp")
 include(":shared")
+include(":compose-web")
