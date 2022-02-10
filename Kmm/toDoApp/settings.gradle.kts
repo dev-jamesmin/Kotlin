a20c10e027c7f1b493b040362d6aa6d9c5324cd9
dev-jamesmin <dev.jamesmin@gmail.com>
2022-02-10 15:56:38 +0900

    [kmm] to do app  "web" 추가.

--- Kmm/toDoApp/settings.gradle.kts
+++ Kmm/toDoApp/settings.gradle.kts
@@ -9,3 +9,17 @@ pluginManagement {
 rootProject.name = "toDoApp"
 include(":androidApp")
 include(":shared")
+include(":web")
+
+
+//include(
+//    ":common:utils",
+//    ":common:database",
+//    ":common:main",
+//    ":common:edit",
+//    ":common:root",
+//    ":common:compose-ui",
+//    ":android",
+//    ":desktop",
+//    ":web"
+//)
