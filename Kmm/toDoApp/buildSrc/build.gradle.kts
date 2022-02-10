0f2e5c0310f189c257587416a09e1487d4d78599
dev-jamesmin <dev.jamesmin@gmail.com>
2022-02-10 15:06:44 +0900

    [kmm] to do app  deps 관련 추가.

--- Kmm/toDoApp/buildSrc/build.gradle.kts
+++ Kmm/toDoApp/buildSrc/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     `kotlin-dsl`
 }
-//
-//initDeps(project)
-//
+
+initDeps(project)
+
 repositories {
     mavenLocal()
     google()
@@ -11,13 +11,13 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
-//dependencies {
-//    implementation(Deps.JetBrains.Compose.gradlePlugin)
-//    implementation(Deps.JetBrains.Kotlin.gradlePlugin)
-//    implementation(Deps.Android.Tools.Build.gradlePlugin)
-//    implementation(Deps.Squareup.SQLDelight.gradlePlugin)
-//}
-//
+dependencies {
+    implementation(Deps.JetBrains.Compose.gradlePlugin)
+    implementation(Deps.JetBrains.Kotlin.gradlePlugin)
+    implementation(Deps.Android.Tools.Build.gradlePlugin)
+    implementation(Deps.Squareup.SQLDelight.gradlePlugin)
+}
+
 //kotlin {
 //    // Add Deps to compilation, so it will become available in main project
 //    sourceSets.getByName("main").kotlin.srcDir("buildSrc/src/main/kotlin")
